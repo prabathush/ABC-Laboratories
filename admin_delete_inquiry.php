54b1fc3b63069096925eeff58760e46a1f84f0be
prabathush <144385360+prabathush@users.noreply.github.com>
2024-03-15 03:18:32 +0530

    Update admin_delete_inquiry.php

--- admin_delete_inquiry.php
+++ admin_delete_inquiry.php
@@ -2,16 +2,30 @@
 
 require_once 'Database.php';
 
+class InquiryDeletion {
+    private $db;
+
+    public function __construct() {
+        // Instantiate Database class
+        $this->db = new Database();
+    }
+
+    public function deleteInquiry($id) {
+        // Delete the inquiry
+        $this->db->query("DELETE FROM inquiries WHERE id = :id", ['id' => $id]);
+    }
+}
+
 // Check if ID parameter is provided
 if (isset($_GET['id'])) {
     // Sanitize the ID
     $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
 
-    // Instantiate Database class
-    $db = new Database();
+    // Instantiate InquiryDeletion object
+    $inquiryDeletion = new InquiryDeletion();
 
     // Delete the inquiry
-    $db->query("DELETE FROM inquiries WHERE id = :id", ['id' => $id]);
+    $inquiryDeletion->deleteInquiry($id);
 
     // Redirect back to inquiries page
     header("Location: admin_dashboard.php");
